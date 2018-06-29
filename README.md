735d1010309ced988432a338275781e266c2715c
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-29 16:32:40 -0700

    going to rebuild now

--- README.md
+++
@@ -1,2 +0,0 @@
-# CodePenToPlayWith-
-A cool codepen I found that I want to learn from and integrate into React , we'll see if that can go 
--- app.js
+++
@@ -1,8 +0,0 @@
-Array.from(document.getElementsByClassName('letter')).forEach(letter => {
-    letter.addEventListener("mouseover", (e) => {
-        letter.classList.add("hovered")
-    })
-    letter.addEventListener("animationend", (e) => {
-        letter.classList.remove("hovered")
-    })
-})
