a78a15ca4fcf3887b0984432aa8f81e5d8f64789
Julian Sandersius <sandersiusjulian@gmail.com>
2018-07-02 09:55:59 -0700

    building out node express server

--- codePen/index.js
+++ codePen/index.js
@@ -1,27 +1,48 @@
-//Load HTTP module
-const express = require('express')
-const app = express();
+// *** Dependencies
+// =============================================================
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
 const http = require("http");
 const hostname = '127.0.0.1';
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('index.html')
-});
 
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
+// Sets up the Express App
+// =============================================================
+const app = express();
+var PORT = process.env.PORT || 8000;
 
-    //Set the response HTPP header with HTTP status and Content type
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
+// Static directory
+app.use(express.static("public"));
 
-//listen for request on port 3000, and as a callback function have the port listened on logged
-app.listen(8000, () => {
-    console.log('Example app listening on port 8000!')
-});
+
+// Routes
+// =============================================================
+module.exports = function (app) {
+
+    // Each of the below routes just handles the HTML page that the user gets sent to.
+
+    // index route loads view.html
+
+    //_________
+    app.get('/', (req, res) => {
+        res.send('index.html')
+    });
+
+    //Create HTTP server and listen on port 3000 for requests
+    const server = http.createServer((req, res) => {
+
+        //Set the response HTPP header with HTTP status and Content type
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello World\n');
+    });
+
+    //listen for request on port 3000, and as a callback function have the port listened on logged
+    app.listen(8000, () => {
+        console.log('Example app listening on port 8000!')
+    });
 
 
 
